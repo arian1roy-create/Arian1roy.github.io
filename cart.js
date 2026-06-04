761927db5a37ec86b6c7b50a5bd97c088ff244cf
arian1roy-create <arian1no1roy@gmail.com>
2026-06-04 06:56:42 +0330

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,79 +1,73 @@
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
-let discount = 0;
+
+function save(){
+localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 function addToCart(name, price){
-cart.push({name, price:Number(price)});
-localStorage.setItem("cart", JSON.stringify(cart));
+cart.push({
+name: name,
+price: Number(price)
+});
+save();
 updateCart();
 }
 
 function removeItem(i){
 cart.splice(i,1);
-localStorage.setItem("cart", JSON.stringify(cart));
-updateCart();
-}
-
-function applyDiscount(code){
-if(code==="OFF10") discount=0.1;
-else if(code==="OFF20") discount=0.2;
-else { alert("Invalid code"); return; }
-
-alert("Discount applied");
+save();
 updateCart();
 }
 
 function updateCart(){
-let box=document.getElementById("cartItems");
-let count=document.getElementById("count");
-let totalBox=document.getElementById("total");
-let finalBox=document.getElementById("finalTotal");
+
+let box = document.getElementById("cartItems");
+let count = document.getElementById("count");
+let totalBox = document.getElementById("total");
 
 if(!box) return;
 
-box.innerHTML="";
-let total=0;
+box.innerHTML = "";
 
-cart.forEach((i,index)=>{
-total+=i.price;
+let total = 0;
 
-box.innerHTML+=`
+cart.forEach((item,i)=>{
+total += Number(item.price);
+
+box.innerHTML += `
 <div style="display:flex;justify-content:space-between;margin:5px 0">
-<span>${i.name}</span>
-<span>
-$${i.price}
-<button onclick="removeItem(${index})" style="background:red;color:white;border:none">x</button>
+<span>${item.name}</span>
+<span>$${item.price}
+<button onclick="removeItem(${i})">x</button>
 </span>
-</div>`;
+</div>
+`;
 });
 
-let final=total-(total*discount);
-
-count.innerText=cart.length;
-totalBox.innerText=total;
-if(finalBox) finalBox.innerText=final.toFixed(0);
-
-localStorage.setItem("cart", JSON.stringify(cart));
+count.innerText = cart.length;
+totalBox.innerText = total;
 }
 
 function toggleCart(){
 let b=document.getElementById("cartBody");
-b.style.display=b.style.display==="block"?"none":"block";
+b.style.display = b.style.display==="block"?"none":"block";
 }
 
 function checkout(){
-if(cart.length===0){ alert("Cart empty"); return; }
+if(cart.length===0){
+alert("Cart is empty");
+return;
+}
 
 let msg="🛒 Order:%0A";
 let total=0;
 
 cart.forEach(i=>{
 msg+=i.name+" - $"+i.price+"%0A";
-total+=i.price;
+total+=Number(i.price);
 });
 
-let final=total-(total*discount);
-
-msg+="%0A💰 Total: $"+final.toFixed(0);
+msg+="%0A💰 Total: $"+total;
 
 window.open("https://t.me/iphonearian?text="+msg,"_blank");
 }
